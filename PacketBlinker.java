0af6fc464573274815cb5fad990f8fded4d3ae63
G-Mr. Xiong <gxiong.mr@gmail.com>
2026-04-04 20:32:57 +0800

    Update PacketBlinker.java

--- PacketBlinker.java
+++ PacketBlinker.java
@@ -1,25 +1,18 @@
 package com.koponat.mditter.network;
 
-import net.fabricmc.fabric.api.client.networking.v1.ClientSendMessageEvents;
-import net.fabricmc.fabric.api.client.networking.v1.C2SPlayChannelEvents;
-import net.minecraft.network.packet.Packet;
-
 public class PacketBlinker {
 
-    private static boolean isBlinking = false;
+    private static boolean active = false;
 
-    public static void setBlinking(boolean value) {
-        isBlinking = value;
+    public static void toggle() {
+        active = !active;
     }
 
-    public static boolean isBlinking() {
-        return isBlinking;
+    public static boolean isActive() {
+        return active;
     }
 
-    public static boolean shouldCancelPacket(Packet<?> packet) {
-        if (isBlinking) {
-            return true;
-        }
-        return false;
+    public static void setActive(boolean state) {
+        active = state;
     }
 }
