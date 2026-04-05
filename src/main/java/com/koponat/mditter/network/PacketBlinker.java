9232bba61b37229da8e5be2f7324552c181974e6
G-Mr. Xiong <gxiong.mr@gmail.com>
2026-04-05 20:16:14 +0800

    Update PacketBlinker.java

--- src/main/java/com/koponat/mditter/network/PacketBlinker.java
+++ src/main/java/com/koponat/mditter/network/PacketBlinker.java
@@ -3,7 +3,7 @@ package com.koponat.mditter.network;
 import net.minecraft.client.network.OtherClientPlayerEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.packet.Packet;
-import net.minecraft.network.packet.c2s.play.KeepAliveC2SPacket;
+import net.minecraft.network.packet.c2s.common.KeepAliveC2SPacket;
 import java.util.UUID;
 
 public class PacketBlinker {
@@ -47,7 +47,7 @@ public class PacketBlinker {
         fakePlayer.copyPositionAndRotation(client.player);
         fakePlayer.headYaw = client.player.headYaw;
         fakePlayer.setUuid(UUID.randomUUID());
-        client.world.addEntity(fakePlayer.getId(), fakePlayer);
+        client.world.addEntity(fakePlayer);
     }
 
     private static void removeFakePlayer() {
