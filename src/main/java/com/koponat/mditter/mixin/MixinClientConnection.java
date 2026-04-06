06ea3593688b87b1c627d985110d6db2123f4b1f
G-Mr. Xiong <gxiong.mr@gmail.com>
2026-04-06 17:02:04 +0800

    Update MixinClientConnection.java

--- src/main/java/com/koponat/mditter/mixin/MixinClientConnection.java
+++ src/main/java/com/koponat/mditter/mixin/MixinClientConnection.java
@@ -11,7 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientConnection.class)
 public class MixinClientConnection {
-    // 注意：1.21.2-1.21.6 的签名通常包含 boolean flush 参数，且可能变为私有引用
     @Inject(at = @At("HEAD"), method = "send(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/PacketCallbacks;Z)V", cancellable = true)
     private void onSend(Packet<?> p, PacketCallbacks c, boolean flush, CallbackInfo ci) {
         if (PacketBlinker.shouldCancel(p)) {
