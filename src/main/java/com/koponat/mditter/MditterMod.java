d9eb0628dc57c82c011840edb4f07d5696a6434e
G-Mr. Xiong <gxiong.mr@gmail.com>
2026-04-06 17:27:17 +0800

    Delete src/main directory

--- src/main/java/com/koponat/mditter/MditterMod.java
+++
@@ -1,9 +0,0 @@
-package com.koponat.mditter;
-
-import net.fabricmc.api.ModInitializer;
-
-public class MditterMod implements ModInitializer {
-    @Override
-    public void onInitialize() {
-    }
-}
