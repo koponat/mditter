51375f578e0a8dd45db08260fa1c507bff517aaf
G-Mr. Xiong <gxiong.mr@gmail.com>
2026-04-08 20:08:19 +0800

    Update MditterMod.java

--- src/main/java/com/koponat/mditter/MditterMod.java
+++ src/main/java/com/koponat/mditter/MditterMod.java
@@ -13,7 +13,6 @@ public class MditterMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        // 注册按键：默认按 J 键打开界面
         openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.mditter.open_gui",
                 InputUtil.Type.KEYSYM,
@@ -21,7 +20,6 @@ public class MditterMod implements ModInitializer {
                 "category.mditter.general"
         ));
 
-        // 注册每一帧的监听
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (openGuiKey.wasPressed()) {
                 client.setScreen(new MditterScreen());
